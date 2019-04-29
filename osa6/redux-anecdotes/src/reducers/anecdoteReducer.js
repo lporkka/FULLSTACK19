2bcf5d5b29314d955ad16cba667b52e34f3b44b7
Lauri <lauri.porkka@hotmail.com>
2019-04-29 17:02:00 +0300

    osa 6.1

---
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -0,0 +1,56 @@
+const anecdotesAtStart = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+
+const getId = () => (100000 * Math.random()).toFixed(0)
+
+const asObject = (anecdote) => {
+  return {
+    content: anecdote,
+    id: getId(),
+    votes: 0
+  }
+}
+
+const sortAnecdotes = (list) => {
+  return list.sort(function (a, b) {
+    return b.votes - a.votes
+  })
+}
+
+export const createVote = (id) => {
+  return { type: 'VOTE', data: { id } }
+}
+
+export const createAnecdote = (content) => {
+  return { type: 'CREATE', data: { content } }
+}
+
+const initialState = anecdotesAtStart.map(asObject)
+
+const reducer = (state = initialState, action) => {
+  console.log('state now: ', state)
+  console.log('action', action)
+
+  switch (action.type) {
+    case 'VOTE':
+      const id = action.data.id
+      const anecdote = state.find(a => a.id === id)
+      anecdote.votes++
+      return sortAnecdotes(state.map(a => a.id === id ? anecdote : a))
+    case 'CREATE':
+      const newAnecdote = action.data.content
+      return state.concat(asObject(newAnecdote))
+    default:
+      return state
+  }
+
+}
+
+export default reducer
